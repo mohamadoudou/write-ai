1d528a4d6b03e8edaeebe6cbbadaba2fe083fda1
moctar <moctarm988@gmail.com>
2023-11-19 21:40:30 +0800

    add suspense

--- app/react-18-demo/page.tsx
+++ app/react-18-demo/page.tsx
@@ -2,6 +2,7 @@
 import {
   ChangeEvent,
   SetStateAction,
+  Suspense,
   memo,
   useDeferredValue,
   useEffect,
@@ -9,34 +10,34 @@ import {
   useTransition,
 } from "react";
 
-const List = memo(function List({ text }: { text: string }) {
-  const [fakeData, setFakeData] = useState<any>([]);
+const List = memo(function List({ fakeData }: { fakeData: any }) {
+  // const [fakeData, setFakeData] = useState<any>([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const responseData = await fetch(
-          "https://jsonplaceholder.typicode.com//posts",
-          {
-            method: "GET",
-            headers: {
-              "Cache-Control": "no-cache",
-              Pragma: "no-cache",
-            },
-          }
-        ).then((response) => response.json());
-        console.log("Iam called", fakeData);
-        setFakeData(responseData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-    fetchData();
-  }, [text]);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const responseData = await fetch(
+  //         "https://jsonplaceholder.typicode.com//posts",
+  //         {
+  //           method: "GET",
+  //           headers: {
+  //             "Cache-Control": "no-cache",
+  //             Pragma: "no-cache",
+  //           },
+  //         }
+  //       ).then((response) => response.json());
+  //       console.log("Iam called", fakeData);
+  //       setFakeData(responseData);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   }
+  //   fetchData();
+  //   console.log("I am called");
+  // }, [text]);
 
   return (
     <div>
-      <div>My text: {text}</div>
       <div>
         <ul>
           {fakeData.map((data: any) => (
@@ -53,7 +54,31 @@ export default function Demo() {
   const [text2, setText2] = useState("");
   const [letters, setLetters] = useState<string[]>([]);
   const [isPending, startTransition] = useTransition();
-  const deferredValueText2 = useDeferredValue(text2);
+  const [fakeData, setFakeData] = useState<any>([]);
+  const deferredValueFakeData = useDeferredValue(fakeData);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const responseData = await fetch(
+          `https://jsonplaceholder.typicode.com//posts?userId=${text2}`,
+          {
+            method: "GET",
+            headers: {
+              "Cache-Control": "no-cache",
+              Pragma: "no-cache",
+            },
+          }
+        ).then((response) => response.json());
+        console.log("Iam called", fakeData);
+        setFakeData(responseData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+    console.log("I am called");
+  }, [text2]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
@@ -83,7 +108,9 @@ export default function Demo() {
             setText2(e.target.value);
           }}
         />
-        <List text={deferredValueText2} />
+        <Suspense fallback={<h2>fetching...</h2>}>
+          <List fakeData={deferredValueFakeData} />
+        </Suspense>
       </div>
       <div>
         <p className="mr-2 mb-2">useTransition Demo</p>
