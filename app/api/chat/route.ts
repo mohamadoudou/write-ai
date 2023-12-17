79cd877b26d6389dbb143df3e0aae08094fbf03b
moctar <moctarm988@gmail.com>
2023-12-17 12:29:50 +0800

    successful intergration with open ai api

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,12 +5,18 @@ export async function GET(req: Request) {
   // Extract the `messages` from the body of the request
   //   const { messages } = await req.json();
 
-  const params: OpenAI.CompletionCreateParams = {
+  const params: OpenAI.Chat.Completions.ChatCompletionMessageParam = {
+    messages: [{ role: "system", content: "Help to make money online." }],
     model: "gpt-3.5-turbo",
-    prompt: "Say this is a test",
-    max_tokens: 6,
-    temperature: 0,
   };
-  const completion: OpenAI.Completion = await openAI.completions.create(params);
+
+  // const completion = await openai.chat.completions.create({
+  //   messages: [{"role": "system", "content": "You are a helpful assistant."},
+  //       {"role": "user", "content": "Who won the world series in 2020?"},
+  //       {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+  //       {"role": "user", "content": "Where was it played?"}],
+  //   model: "gpt-3.5-turbo",
+  // });
+  const completion = await openAI.chat.completions.create(params);
   return Response.json(completion);
 }
